44958272262a481d63470b2a14cfdd53cc91fbcb
fgasfga <75693117+fgasfga@users.noreply.github.com>
2022-03-23 11:42:12 +0300

    Create LoadString.lua

---
+++ LoadString.lua
@@ -0,0 +1,4 @@
+--[[Game:https://www.roblox.com/games/4522347649/FREE-ADMIN#!/game-instances]]--
+--[[GamePass needed:https://www.roblox.com/game-pass/7846315/Super-Admin]]--
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fgasfga/Free-Admin-Script/main/Main.lua", true))()
